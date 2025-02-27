b80e818919c504e5229ce2ea0f183635884c42ac
Abhiaws26 <abhivshek26@gmail.com>
2025-02-27 14:19:47 +0530

    updated name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Apexon'
+    return 'This is Abhishek Welcome to Apexon'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
